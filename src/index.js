430122ec5876cba818d7f2de40b2ce58e8535943
Taku-Yamashita <jbay@outlook.jp>
2020-11-30 23:03:33 +0900

    update

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const path = require("path")
 
 async function run() {
   try {
-    await io.rmRF('images')
-    await io.mkdirP('images')
+    await io.rmRF('images/old.png');
+    await io.mv('images/current.png', 'images/old.png');
     const url = core.getInput('url')
-    const now = moment().tz("Asia/Tokyo").format('YYYY-MM-DD_HH:mm:ss')
-    const dest = `images/${now}.png`
+   //const now = moment().tz("Asia/Tokyo").format('YYYY-MM-DD_HH:mm:ss')
+    const dest = `images/current.png`
     core.info(`fetch ${url} screenshot`);
-    core.info(`file name ${now}.png`)
+    //core.info(`file name ${now}.png`)
 
     const browser = await puppeteer.launch({
       executablePath: path.normalize("/usr/bin/google-chrome")
