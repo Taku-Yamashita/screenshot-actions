428f7e86997bca03febeb7fb8bbbd55ea7ba3c50
Taku-Yamashita <jbay@outlook.jp>
2020-12-01 00:20:26 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 const core = require('@actions/core')
-/*
+
 const io = require('@actions/io');
 const fs = require('fs');
 const old = 'images/old.png'
 const dest = `images/current.png`
 const { screenshot } = require('./page')
-const { report } = require('./lighthouse')
-*/
+//const { report } = require('./lighthouse')
+
 async function run() {
   try {
     core.info(`start`);
     const url = core.getInput('url')
-    /*
     await io.rmRF(old);
     if(fs.existsSync(dest)) {
       await io.mv(dest, old);
     }
     core.info(`fetch ${url} screenshot`);
     await screenshot(url, dest);
-     */
     //await report(url);
 
 
