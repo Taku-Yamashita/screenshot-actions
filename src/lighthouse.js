24a76e7ef62043fd6698c72e149dc58c1fc5c7a1
Taku-Yamashita <jbay@outlook.jp>
2020-11-30 23:58:28 +0900

    update

--- src/lighthouse.js
+++ src/lighthouse.js
@@ -1,8 +1,10 @@
+const core = require('@actions/core')
 const fs = require('fs');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 export async function report(url) {
+    core.info(`fetch ${url} score`);
     const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
     const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
     const runnerResult = await lighthouse(url, options);
@@ -12,8 +14,8 @@ export async function report(url) {
     fs.writeFileSync('lighthouse/report.html', reportHtml);
 
     // `.lhr` is the Lighthouse Result as a JS object
-    console.log('Report is done for', runnerResult.lhr.finalUrl);
-    console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+    core.info('Report is done for', runnerResult.lhr.finalUrl);
+    core.info('Performance score was', runnerResult.lhr.categories.performance.score * 100);
 
     await chrome.kill();
 }
