459b1dcd35986f4a8af285074fbdf0ec4e8288bc
Taku-Yamashita <jbay@outlook.jp>
2021-03-28 15:19:39 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const webpack = require("webpack");
 const webpackConfig = require("./webpack.config");
 
 gulp.task("default", () => {
-    // ☆ webpackStreamの第2引数にwebpackを渡す☆
     return webpackStream(webpackConfig, webpack)
         .pipe(gulp.dest("dist"));
 })
